178607333e414b705e7c84cef50de5160acb1548
Muhammed Emin Çağlar <100859025+Caglaar@users.noreply.github.com>
2023-02-20 12:00:37 +0300

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcaglar <m>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 16:54:58 by mcaglar           #+#    #+#             */
+/*   Updated: 2023/02/19 17:18:15 by mcaglar          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && s1[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+void	ft_sort_ascii(char *str[], int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (ft_strcmp(str[j], str[j + 1]) > 0)
+				ft_swap(&str[j], &str[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		ft_sort_ascii(argv + 1, argc -1);
+		while (argv[i])
+		{
+			ft_putstr(argv[i]);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
