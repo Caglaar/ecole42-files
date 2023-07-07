28aca5922602e59ac57b76f514b3ac08df6e7a6e
Muhammed Emin Caglar <mcaglar@k2m08s01.42kocaeli.com.tr>
2023-07-07 11:26:50 +0300

    Libft project finished

---
+++ Cursus/Libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcaglar <m>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 10:48:51 by mcaglar           #+#    #+#             */
+/*   Updated: 2023/07/07 11:14:31 by mcaglar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*temp;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new = NULL;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&temp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, temp);
+		lst = lst->next;
+	}
+	return (new);
+}
